562b12796a672cf9ce05d5dac3f11bba9c492766
Felipe Batista da Silva <felipe@batistadasilva.com>
2023-11-10 13:46:51 +0100

    feat: deployment config not required

--- src/scripts/deploy-bundle.sh
+++ src/scripts/deploy-bundle.sh
@@ -5,23 +5,34 @@ ORB_EVAL_BUNDLE_BUCKET="$(eval echo "${ORB_EVAL_BUNDLE_BUCKET}")"
 ORB_EVAL_BUNDLE_KEY="$(eval echo "${ORB_EVAL_BUNDLE_KEY}")"
 ORB_EVAL_REGION="$(eval echo "${ORB_EVAL_REGION}")"
 
-if [ -n "${ORB_VAL_GET_DEPLOYMENT_GROUP_ARGUMENTS}" ]; then 
+if [ -n "${ORB_VAL_GET_DEPLOYMENT_GROUP_ARGUMENTS}" ]; then
   set -- "$@" "${ORB_VAL_GET_DEPLOYMENT_GROUP_ARGUMENTS}"
 fi
 
 if [ -n "${ORB_VAL_DEPLOY_BUNDLE_ARGUMENTS}" ]; then
   set -- "$@" "${ORB_VAL_DEPLOY_BUNDLE_ARGUMENTS}"
-fi 
+fi
 
-ID=$(aws deploy create-deployment \
-    --application-name "${ORB_EVAL_APPLICATION_NAME}" \
-    --deployment-group-name "${ORB_EVAL_DEPLOYMENT_GROUP}" \
-    --deployment-config-name "${ORB_VAL_DEPLOYMENT_CONFIG}" \
-    --region "${ORB_EVAL_REGION}" \
-    --s3-location bucket="${ORB_EVAL_BUNDLE_BUCKET}",bundleType="${ORB_VAL_BUNDLE_TYPE}",key="${ORB_EVAL_BUNDLE_KEY}"."${ORB_VAL_BUNDLE_TYPE}" \
-    --profile "${ORB_VAL_PROFILE_NAME}" \
-    --output text \
-    --query '[deploymentId]' "${ORB_VAL_DEPLOY_BUNDLE_ARGUMENTS}")
+if [ -n "${ORB_STR_DEPLOYMENT_CONFIG}" ]; then
+  ID=$(aws deploy create-deployment \
+      --application-name "${ORB_STR_APPLICATION_NAME}" \
+      --deployment-group-name "${ORB_STR_DEPLOYMENT_GROUP}" \
+      --region "${ORB_STR_REGION}" \
+      --s3-location bucket="${ORB_STR_BUNDLE_BUCKET}",bundleType="${ORB_STR_BUNDLE_TYPE}",key="${ORB_STR_BUNDLE_KEY}"."${ORB_STR_BUNDLE_TYPE}" \
+      --profile "${ORB_STR_PROFILE_NAME}" \
+      --output text \
+      --query '[deploymentId]' "${ORB_STR_DEPLOY_BUNDLE_ARGUMENTS}")
+else
+  ID=$(aws deploy create-deployment \
+      --application-name "${ORB_STR_APPLICATION_NAME}" \
+      --deployment-group-name "${ORB_STR_DEPLOYMENT_GROUP}" \
+      --deployment-config-name "${ORB_STR_DEPLOYMENT_CONFIG}" \
+      --region "${ORB_STR_REGION}" \
+      --s3-location bucket="${ORB_STR_BUNDLE_BUCKET}",bundleType="${ORB_STR_BUNDLE_TYPE}",key="${ORB_STR_BUNDLE_KEY}"."${ORB_STR_BUNDLE_TYPE}" \
+      --profile "${ORB_STR_PROFILE_NAME}" \
+      --output text \
+      --query '[deploymentId]' "${ORB_STR_DEPLOY_BUNDLE_ARGUMENTS}")
+fi
 STATUS=$(aws deploy get-deployment \
     --deployment-id "$ID" \
     --output text \
